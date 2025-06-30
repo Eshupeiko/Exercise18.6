cc75330a485ec8e801a00fce43ae59f3c5436398
Evgeny <e.shupeiko@gmail.com>
2025-06-30 22:45:40 +0300

    A new file added

---
+++ less1.py
@@ -0,0 +1,20 @@
+# создаем словарь
+translator = {'bug': 'ошибка', 'function': 'функция', 'approve': 'согласовать'}
+# выводим английские слова, которые сохранены в словаре
+print(f'Слова в словаре: {translator.keys()}')
+
+# считываем слово, которое нужно перевести
+eng_word = input('Введите слово, которое нужно удалить: ')
+if eng_word in translator.keys():
+    del translator[eng_word]
+    print('Задача выполнена')
+    for key, value in translator.items():
+        # выводим пары ключ - значение
+        print(f'{key} - {value}')
+    print(f'Отредактированный словарь:')
+else:
+    print("Этого слова нет в словаре")
+    for key, value in translator.items():
+            # выводим пары ключ - значение
+            print(f'{key} - {value}')
+    print(f'Исходный словарь:')
