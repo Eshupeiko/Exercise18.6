38204362376d25937fc3236585b6452435e7956f
Evgeny <e.shupeiko@gmail.com>
2025-07-01 12:46:37 +0300

    DEL

--- less22.py
+++
@@ -1,9 +0,0 @@
-# создаем словарь
-translator = {'bug': 'ошибка', 'function': 'функция', 'approve': 'согласовать'}
-# выводим английские слова, которые сохранены в словаре
-print(f'Слова в словаре: {translator.keys()}')
-
-# считываем слово, которое нужно перевести
-eng_word = input('Введите слово, которое нужно удалить: ')
-if eng_word in translator.keys():
-    del translator[eng_word]
